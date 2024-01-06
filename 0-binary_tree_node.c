9394b26f5f6c57ef15b4361d602b28f195393a32
eneamilo514 <eneamilo514@gmail.com>
2024-01-06 15:13:50 -0800

    o floro e bona te paren

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,20 @@
+#include"binary_trees.h"
+#include<stdlib.h>
+
+/**
+ * binary_tree_node -creates a new binary tree node
+ * @parent: -points to the parent node
+ * @value: -the value to be inserted into the node
+ * Return (newnode)
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newnode = malloc(sizeof(binary_tree_t));
+
+	if (newnode == NULL)
+		return (NULL);
+	newnode->parent = parent;
+	newnode->n = value;
+	return (newnode);
+}
