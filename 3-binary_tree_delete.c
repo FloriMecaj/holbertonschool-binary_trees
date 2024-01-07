7610e8925ac656660bd405c30e53c3b65899f38a
eneamilo514 <eneamilo514@gmail.com>
2024-01-07 11:25:18 -0800

    another file

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,13 @@
+#include"binary_trees.h"
+#include<stdlib.h>
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+	if (tree->right != NULL)
+		binary_tree_delete(tree->right);
+	if (tree->left != NULL)
+		binary_tree_delete(tree->left);
+	free(tree);
+}
