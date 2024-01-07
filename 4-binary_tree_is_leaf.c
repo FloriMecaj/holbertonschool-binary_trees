1eda9981176af6482c6ffbd84914af586bffe3ab
FloriMecaj <florimecaj31@gmail.com>
2024-01-07 21:15:49 +0000

    check if node is leaf

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -13,8 +13,4 @@ int binary_tree_is_leaf(const binary_tree_t *node)
         return (1);
     else
         return (0);
-    //if (node->right == NULL)
-        //return (1);
-    //else
-        //return (0);
 }
