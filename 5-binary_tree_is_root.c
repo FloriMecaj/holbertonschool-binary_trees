a71ed622beaf188ccbb59b5588ef7d8ecd388a30
FloriMecaj <florimecaj31@gmail.com>
2024-01-07 21:22:34 +0000

    check if node is root

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/*
+* binary_tree_is_root - ssssssss
+* @node: ajahcadjbv
+* Return: seuybv
+*/
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+    if (node->left != NULL && node->right != NULL)
+        return (1);
+    else
+        return (0);
+}
