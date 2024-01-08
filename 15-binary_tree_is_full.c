82fb42a17ab91aa8c2eb08f5415fb0b30b7edd62
eneamilo514 <eneamilo514@gmail.com>
2024-01-08 02:34:24 -0800

    updates

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,14 +1,17 @@
 #include"binary_trees.h"
 
+/*
+ * binary_tree_is_full - hgjvhbadv
+ * @tree: fgvhbjn
+ * Return: agjvh
+ */
+
+
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-    if (tree == NULL) {
-        return 1;  // Empty tree is full
-    }
+	if (tree == NULL)
+		return (0);
 
-    if (tree->left == NULL || tree->right == NULL) {
-        return 0;  // Not full if either child is missing
-    }
-
-    return binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right);
+	if ((tree->left == NULL && tree->right != NULL) && (tree->left != NULL && tree->right == NULL))
+		return (0);
 }
