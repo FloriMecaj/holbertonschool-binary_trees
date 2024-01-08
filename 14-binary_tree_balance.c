9e0275eec8ddac7afd4bfe95f0d829a92e3489a9
FloriMecaj <florimecaj31@gmail.com>
2024-01-08 22:15:34 +0000

    updates

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,28 +8,20 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        size_t left_edges = 0;
-        size_t right_edges = 0;
-        size_t height = 0;
+    size_t left_edges = 0;
+    size_t right_edges = 0;
+    size_t height = 0;
 
-        if (tree == NULL || (tree->left ==NULL && tree->right == NULL))
-                return (0);
-        if (tree->left != NULL)
-                left_edges = binary_tree_height(tree->left);
-        if (tree->right != NULL)
-                right_edges = binary_tree_height(tree->right);
-        if (left_edges > right_edges)
-        {
-                height = left_edges + 1;
-                return (height);
-        }
-        else
-        {
-                height = right_edges + 1;
-                return (height);
-        }
-
+    if (tree == NULL)
+        return (-1);
+    if (tree->left != NULL)
+        left_edges = binary_tree_height(tree->left) + 1;
+    if (tree->right != NULL)
+        right_edges = binary_tree_height(tree->right) + 1;
+    height = (left_edges > right_edges) ? left_edges : right_edges;
+    return (height);
 }
+
 /**
  * binary_tree_balance -finds the balance of a binary tree
  * @tree: -points to the root of the tree
@@ -43,7 +35,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 	int balance;
 
 	if (tree == NULL)
-		return (0);
+                return (0);
 	left_edges = (int) binary_tree_height(tree->left);
 	right_edges = (int) binary_tree_height(tree->right);
 	balance = left_edges - right_edges;
